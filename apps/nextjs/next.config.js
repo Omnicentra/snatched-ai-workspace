42d5ea24b95eaf5b97ff606906dc73ac547467df
Chipzstar <chipzstar.dev@googlemail.com>
2025-05-09 13:30:05 +0100

    enable instrumentationHook

--- apps/nextjs/next.config.js
+++ apps/nextjs/next.config.js
@@ -19,6 +19,9 @@ const config = {
   /** We already do linting and typechecking as separate tasks in CI */
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
+  experimental: {
+    instrumentationHook: true,
+  },
 };
 
 export default withSentryConfig(config, {
